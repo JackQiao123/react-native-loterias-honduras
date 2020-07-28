6fc56f7a1d50eb5adf7b0d15714cdcc640f05a67
JackQiao123 <jack.qiao123@gmail.com>
2020-07-29 04:18:39 +0700

    Project Full Commit

---
+++ config/index.js
@@ -0,0 +1,69 @@
+import { Platform } from 'react-native';
+
+const config = {
+  API_ENDPOINTS: {
+    MAIN: 'https://loteriasdehonduras.com/mobile-api-new',
+    APP: 'https://loteriasdehonduras.com/mobile-api-new'
+  },
+
+  // ADMOB
+  ADMOB: {
+    REWARDED_MAX: 4,
+    SECRETS: Platform.select({
+      ios: {
+        BANNER: 'ca-app-pub-1196303242456869/1822774033',
+        INTERSTITIAL: 'ca-app-pub-1196303242456869/9595316835'
+      },
+      android: {
+        BANNER: 'ca-app-pub-1196303242456869/7869307636',
+        INTERSTITIAL: 'ca-app-pub-1196303242456869/8118583631'
+      }
+    })
+  },
+
+  SETTINGS: {
+    ENCRYPT: true,
+    REFRESH_INTERVAL: 30000,
+    APP_NAME: 'app_honduras'
+  },
+
+  VIEW_OPTIONS: {
+    THEME: 'blue1',
+    INITIAL_MENU: {
+      ID: 'home',
+      OPTIONS: {
+        toggle: true,
+        trigger: true
+      }
+    },
+    MENUS: {
+      SHOW_COMPANIES: true
+    },
+    LAYOUTS: {
+      LOGO_ASPECT_RATIO: 247 / 110,
+      SHOW_ALL_GAMES_AT_COMPANY: true,
+      BREED_CRUMB_COMPANY_DISTINCTION: true,
+      MENU_PRIMARY_DISTINCTION: true
+    },
+    NAVIGATIONS: {
+      SHOW_TITLE: true
+    }
+  },
+  ENUMS: {
+    SCREEN_TYPE: {
+      HOME: 1,
+      MENU: 2,
+      GAME: 3,
+    },
+    MENU_TYPE: {
+      PRIMARY: 1,
+      GAME: 2,
+    }
+  },
+
+  VARIABLES: {
+    app: null
+  }
+};
+
+export default config;
